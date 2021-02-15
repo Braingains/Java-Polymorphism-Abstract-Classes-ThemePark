6aaf0f4cfa8188318617a544d9348c555ff87771
Ewan Brain <ewan.brain@gmail.com>
2021-02-15 19:06:41 +0000

    tobaccoStall tests set up

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor Earnest_Oldman;
+    Visitor Nooby_Youngman;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        Earnest_Oldman = new Visitor(100, 160, 600.0);
+        Nooby_Youngman = new Visitor(5, 100, 5.50);
     }
 
     @Test
@@ -27,5 +32,14 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void under18sCannotBuyCigarettes() {
+        assertEquals(false, tobaccoStall.isAllowedTo(Nooby_Youngman));
+    }
+    @Test
+    public void over18sCanBuyCigarettes() {
+        assertEquals(true, tobaccoStall.isAllowedTo(Earnest_Oldman));
     }
 }
