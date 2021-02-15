882982dc72532fb528d9e6557320ba33df9bac7d
Ewan Brain <ewan.brain@gmail.com>
2021-02-15 19:00:44 +0000

    ISecurity tests set up

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor Earnest_Oldman;
+    Visitor Nooby_Youngman;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        Earnest_Oldman = new Visitor(100, 160, 600.0);
+        Nooby_Youngman = new Visitor(5, 100, 5.50);
     }
 
     @Test
@@ -27,4 +32,15 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void under16sCanEnter() {
+        assertEquals(true, playground.isAllowedTo(Nooby_Youngman));
+    }
+    @Test
+    public void over15sCannotEnter() {
+        assertEquals(false, playground.isAllowedTo(Earnest_Oldman));
+    }
+
+
 }
