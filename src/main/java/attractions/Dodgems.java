da1a06c349e408f7629028552488d27156019532
Ewan Brain <ewan.brain@gmail.com>
2021-02-15 18:44:25 +0000

    ITicketed methods added and tested

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,24 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
+    private double defaultPrice;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+        this.defaultPrice = 4.50;
+    }
+
+    public double defaultPrice() {
+        return this.defaultPrice;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if (visitor.getHeight() >= 200) {
+            return (this.defaultPrice * 2);
+        }
+        return defaultPrice();
     }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor Hugh_Tallman;
+    Visitor Mousey_Smallman;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        Hugh_Tallman = new Visitor(20, 201, 100.0);
+        Mousey_Smallman = new Visitor(30, 120, 150.0);
     }
 
 
@@ -29,4 +34,18 @@ public class DodgemTest {
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
     }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+    @Test
+    public void normalPeoplePayDefaultPrice() {
+        assertEquals(4.50, dodgems.priceFor(Mousey_Smallman), 0.01);
+    }
+    @Test
+    public void tallPeopleAreDiscriminatedAgainst() {
+        assertEquals(9.00, dodgems.priceFor(Hugh_Tallman), 0.01);
+    }
+
 }
